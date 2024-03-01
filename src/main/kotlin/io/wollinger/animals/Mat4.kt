29c7565821322f59e5e1bc520f2f9d314d6e43d2
SvenWollinger <accounts@wollinger.email>
2024-03-01 12:29:31 +0000

    Deploying to gh-pages from  @ 4adbea3584aa47478c7005ec85771d0e5fce4da5 🚀

--- src/main/kotlin/io/wollinger/animals/Mat4.kt
+++
@@ -1,5 +0,0 @@
-@JsModule("gl-matrix")
-@JsNonModule
-external val glMatrix: dynamic
-
-val mat4: dynamic = glMatrix.mat4
--- src/main/kotlin/io/wollinger/animals/utils/Logging.kt
+++
@@ -1,6 +0,0 @@
-package io.wollinger.animals.utils
-
-fun error(message: Any) = console.error(message)
-fun info(message: Any) = console.info(message)
-fun warn(message: Any) = console.warn(message)
-fun debug(message: Any) = console.info("[debug] $message")
