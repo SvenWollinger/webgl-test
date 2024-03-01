90e6b39c4a55e7ff66c90d7789d5645da77674dd
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2024-03-01 12:52:09 +0100

    Update default.fragment.glsl

--- src/main/resources/shaders/default.fragment.glsl
+++ src/main/resources/shaders/default.fragment.glsl
@@ -4,5 +4,8 @@ varying vec3 vColor;
 uniform sampler2D uSampler;
 
 void main() {
-    gl_FragColor = texture2D(uSampler, vTextureCoord) * vec4(vColor, 1.0);
+    //gl_FragColor = texture2D(uSampler, vTextureCoord) * vec4(vColor, 1.0);
+    vec4 pixel = texture2D(uSampler, vTextureCoord) * vec4(vColor, 1.0);
+    if (pixel.a <= 0.0) discard;
+    gl_FragColor = pixel;
 }
